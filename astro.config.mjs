8a04ab37b99503525555ac6d7111a6649c20daaa
PabloTech25 <munozjuanpablo424@gmail.com>
2026-02-14 12:10:04 -0600

    Se implementan mejoras para optener buen SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chaudglace.com',
   integrations: [svelte()],
   vite: {
     plugins: [tailwindcss()]
